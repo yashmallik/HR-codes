b9543cf27d3f488e64b1f93ba9ab00b4685007b8
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-07-04 13:55:53 +0530

    Update Day 4.py

--- Day 4.py
+++ Day 4.py
@@ -1,3 +1,12 @@
+# Task
+# Write a Person class with an instance variable, age, and a constructor that takes an integer, intialAge, as a parameter. The constructor must assign intialAge to age after confirming the argument passed as intialAge is not negative; if a negative argument is passed as intialAge, the constructor should set  to  and print Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
+
+# yearPasses() should increase the  instance variable by .
+# amIOld() should perform the following conditional actions:
+# If age<13 , print You are young..
+# Ifage >= 13  and age < 18, print You are a teenager..
+# Otherwise, print You are old..
+# To help you learn by example and complete this challenge, much of the code is provided for you, but you'll be writing everything in the future. The code that creates each instance of your Person class is in the main method. Don't worry if you don't understand it all quite yet!
 class Person:
     def __init__(self,initialAge):
         if initialAge<0:
