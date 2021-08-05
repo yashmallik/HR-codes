83c2a52b6d42d83ba05398f66819dbb111c68337
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-08-05 23:59:49 +0530

    Create Day 23.py

---
+++ Day 23.py
@@ -0,0 +1,25 @@
+# Objective
+# Today, we're going further with Binary Search Trees. Check out the Tutorial tab for learning materials and an instructional video!
+
+# Task
+# A level-order traversal, also known as a breadth-first search, visits each level of a tree's nodes from left to right, top to bottom. You are given a pointer,root , pointing to the root of a binary search tree. Complete the levelOrder function provided in your editor so that it prints the level-order traversal of the binary search tree.
+
+# Hint: You'll find a queue helpful in completing this challenge.
+
+# Function Description
+
+# Complete the levelOrder function in the editor below.
+
+# levelOrder has the following parameter:
+# - Node pointer root: a reference to the root of the tree
+
+
+
+    def levelOrder(self,root):
+        #Write your code here
+        queue = [root]
+        if root != None:
+            out = queue.pop()
+            print(out)
+            
+        
