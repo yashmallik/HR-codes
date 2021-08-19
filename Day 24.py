936c5aaf576b73c6143e39bbe9996b7bc75eea69
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-08-19 22:24:48 +0530

    Create Day 24.py

---
+++ Day 24.py
@@ -0,0 +1,9 @@
+#  Objective
+# Check out the Tutorial tab for learning materials and an instructional video!
+
+# Task
+# A Node class is provided for you in the editor. A Node object has an integer data field, Data, and a Node instance pointer, next, pointing to another node (i.e.: the next node in a list).
+
+# A removeDuplicates function is declared in your editor, which takes a pointer to the head node of a linked list as a parameter. Complete removeDuplicates so that it deletes any duplicate nodes from the list and returns the head of the updated list.
+
+# Note: The head pointer may be null, indicating that the list is empty. Be sure to reset your next pointer when performing deletions to avoid breaking the list.
