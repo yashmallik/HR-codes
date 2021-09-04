617c1fd54df0de14f20350be2ec3502b441b66f5
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-09-04 23:37:15 +0530

    Update Day 28.py

--- Day 28.py
+++ Day 28.py
@@ -1,3 +1,13 @@
+
+# Objective
+# Today, we're working with regular expressions. Check out the Tutorial tab for learning materials and an instructional video!
+
+# Task
+# Consider a database table, Emails, which has the attributes First Name and Email ID. Given N rows of data simulating the Emails table, print an alphabetically-ordered list of people whose email address ends in @gmail.com.
+
+--------------------X--------------------X--------------------X--------------------X--------------------X--------------------X--------------------X--------------------X--------------------X--------------------X--------------------X--------------------X
+--------------------X--------------------X--------------------X--------------------X--------------------X--------------------X--------------------X--------------------X--------------------X--------------------X--------------------X--------------------X
+
 #!/bin/python3
 
 import math
