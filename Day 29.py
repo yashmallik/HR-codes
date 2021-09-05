ff69661ff0380e239d965b238ef36e0b2555c6e6
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-09-05 20:39:04 +0530

    Update Day 29.py

--- Day 29.py
+++ Day 29.py
@@ -1,16 +1,16 @@
-Objective
-Welcome to the last day! Today, we're discussing bitwise operations. Check out the Tutorial tab for learning materials and an instructional video!
+# Objective
+# Welcome to the last day! Today, we're discussing bitwise operations. Check out the Tutorial tab for learning materials and an instructional video!
 
-Task
-Given set S={1,2,3,4,5.......N}. Find two integers, A and B (where A<B), from set S such that the value of A&B is the maximum possible and
-also less than a given integer, K. In this case, & represents the bitwise AND operator.
+# Task
+# Given set S={1,2,3,4,5.......N}. Find two integers, A and B (where A<B), from set S such that the value of A&B is the maximum possible and
+# also less than a given integer, K. In this case, & represents the bitwise AND operator.
 
-Function Description
-Complete the bitwiseAnd function in the editor below.
+# Function Description
+# Complete the bitwiseAnd function in the editor below.
 
-bitwiseAnd has the following paramter(s):
-- int N: the maximum integer to consider
-- int K: the limit of the result, inclusive
+# bitwiseAnd has the following paramter(s):
+# - int N: the maximum integer to consider
+# - int K: the limit of the result, inclusive
 
 ----------------------X----------------------X----------------------X----------------------X----------------------X----------------------X----------------------X----------------------X----------------------X
 ----------------------X----------------------X----------------------X----------------------X----------------------X----------------------X----------------------X----------------------X----------------------X
